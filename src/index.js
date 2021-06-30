4841b1479b3eeca3b2d486cbfa310144344b2af4
leyosu23 <leyosu23@gmail.com>
2021-06-30 15:35:24 -0400

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const element  = <h1>Hello</h1>
+console.log(element);
+ReactDOM.render(element,document.getElementById('root'));
